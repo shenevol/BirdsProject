2da390321c8b999f1ef303719b20fe18f8da906e
yuting fu <cat3067@gmail.com>
2016-03-31 12:03:40 +0800

    Initial Commit

---
+++ Exercise1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exercise1
+//
+//  Created by cat3067 on 2016/3/31.
+//  Copyright © 2016年 yutingfu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
